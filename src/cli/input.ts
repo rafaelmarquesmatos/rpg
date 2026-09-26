d53ddecef4cfd64ae6e10c294f360a9a5f3088e6
Ta1Dev <rthauan171@gmail.com>
2026-09-25 22:18:04 -0400

    refactor(cli): separa entrada e saida do terminal

---
+++ src/cli/input.ts
@@ -0,0 +1,28 @@
+import { createInterface } from "node:readline/promises"
+import { stdin as input, stdout as output } from "node:process"
+
+/**
+ * @class Input contera as configurações de terminal considerando principios de esncapsulamento
+ */
+
+export class Input{
+    //* gera a interface de entrada e saida no terminal
+    private rl = createInterface({
+        input,
+        output
+    })
+    //* espera e promete entregar ums string em algum momento
+    async receberMensgen(): Promise<string>{        
+        return await this.rl.question("> ")
+    }
+    //! importante para liberar o recurso num esqueminha como "abrir -> usar -> liberar"
+    fechar(){
+        //* fecha o readline
+        this.rl.close()
+        //* deixa de receber dados da exucução do stdin
+        input.pause()
+        //* permite que o processo termine mesmo que stdin ainda exista como handle ativo
+        input.unref()
+        console.log("Loop encerrado.")
+    }
+}
